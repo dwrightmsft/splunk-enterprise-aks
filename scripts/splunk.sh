7a4249a8386bab7a8632ab5b80ae9d54efbe6bd4
dwrightmsft <dawrig@microsoft.com>
2021-04-13 13:16:44 +0100

    Echo license for testing

--- scripts/splunk.sh
+++ scripts/splunk.sh
@@ -4,6 +4,7 @@ az aks install-cli
 # Get minio cluster credentials
 az aks get-credentials -g $RESOURCEGROUP -n $CLUSTERNAME
 # Create License file
+echo $LICENSEFILE
 echo $LICENSEFILE | base64 -di > /tmp/Splunk.License
 # Add license to configmap
 kubectl create configmap splunk-licenses --from-file=/tmp/Splunk.License
