bcd32aa1b241b74c7640f222100658ff0afd36ad
dwrightmsft <dawrig@microsoft.com>
2021-04-13 13:06:38 +0100

    Update license path

--- scripts/splunk.sh
+++ scripts/splunk.sh
@@ -3,12 +3,10 @@
 az aks install-cli
 # Get minio cluster credentials
 az aks get-credentials -g $RESOURCEGROUP -n $CLUSTERNAME
-# Create namespace
-kubectl create namespace splunk
 # Create License file
-echo $LICENSEFILE | base64 -di > Splunk.License
+echo $LICENSEFILE | base64 -di > /tmp/Splunk.License
 # Add license to configmap
-kubectl create configmap splunk-licenses --from-file=Splunk.License
+kubectl create configmap splunk-licenses --from-file=/tmp/Splunk.License
 # Install Splunk Operator
 kubectl apply -f $SPLUNKOPERATORURL
 kubectl wait --for condition="established" crd --all
