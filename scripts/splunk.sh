6dd5b50f9966c9b98ac709609f485e9c010e3aca
dwrightmsft <dawrig@microsoft.com>
2021-04-14 07:47:08 +0100

    Test remove -i option

--- scripts/splunk.sh
+++ scripts/splunk.sh
@@ -4,15 +4,16 @@ az aks install-cli
 # Get minio cluster credentials
 az aks get-credentials -g $RESOURCEGROUP -n $CLUSTERNAME
 # Create License file
-echo $LICENSEFILE
-which base64
-echo $LICENSEFILE | base64 -di
-echo $LICENSEFILE | base64 -di > /tmp/Splunk.License
-#cat /tmp/Splunk.License
+echo "LicInput: $LICENSEFILE"
+LICENSEFILE=$(echo $LICENSEFILE | tr -d '[:blank:]\n')
+echo "LicInputTrimmed: $LICENSEFILE"
+echo $LICENSEFILE | base64 -d
+echo "LicDecoded: $LICENSEFILE"
+echo $LICENSEFILE | base64 -d > /tmp/Splunk.License
 # Add license to configmap
-#kubectl create configmap splunk-licenses --from-file=/tmp/Splunk.License
+kubectl create configmap splunk-licenses --from-file=/tmp/Splunk.License
 # Install Splunk Operator
-#kubectl apply -f $SPLUNKOPERATORURL
-#kubectl wait --for condition="established" crd --all
+kubectl apply -f $SPLUNKOPERATORURL
+kubectl wait --for condition="established" crd --all
 # Install Splunk
-#kubectl apply -f $SPLUNKDEPLOYMENTYAML
+kubectl apply -f $SPLUNKDEPLOYMENTYAML
