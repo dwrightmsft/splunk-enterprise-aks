3c0904cf61016d145b80fd58b6ad69ea97903a01
dwrightmsft <dawrig@microsoft.com>
2021-04-14 19:59:20 +0100

    Target agentpool and namespace

--- scripts/splunk.sh
+++ scripts/splunk.sh
@@ -4,14 +4,12 @@ az aks install-cli
 # Get minio cluster credentials
 az aks get-credentials -g $RESOURCEGROUP -n $CLUSTERNAME
 # Create License file
-echo "LicInput: $LICENSEFILE"
 LICENSEFILE=$(echo $LICENSEFILE | tr -d '[:blank:]\n')
-echo "LicInputTrimmed: $LICENSEFILE"
-echo $LICENSEFILE | base64 -d
-echo "LicDecoded: $LICENSEFILE"
 echo $LICENSEFILE | base64 -d > /tmp/Splunk.License
+# Create namespace
+kubectl create namespace splunk --dry-run -o yaml | kubectl apply -f -
 # Add license to configmap
-kubectl create configmap splunk-licenses --from-file=/tmp/Splunk.License
+kubectl create configmap --namespace splunk splunk-licenses --from-file=/tmp/Splunk.License --dry-run -o yaml | kubectl apply -f -
 # Install Splunk Operator
 kubectl apply -f $SPLUNKOPERATORURL
 kubectl wait --for condition="established" crd --all
