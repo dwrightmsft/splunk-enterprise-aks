af0c0efb247432d8cd8c5fee20db5a5902d0422d
dwrightmsft <dawrig@microsoft.com>
2021-04-15 11:23:02 +0100

    Testing cluster wide install

--- scripts/splunk.sh
+++ scripts/splunk.sh
@@ -6,12 +6,33 @@ az aks get-credentials -g $RESOURCEGROUP -n $CLUSTERNAME
 # Create License file
 LICENSEFILE=$(echo $LICENSEFILE | tr -d '[:blank:]\n')
 echo $LICENSEFILE | base64 -d > /tmp/Splunk.License
-# Create namespace
-kubectl create namespace splunk --dry-run -o yaml | kubectl apply -f -
-# Add license to configmap
-kubectl create configmap --namespace splunk splunk-licenses --from-file=/tmp/Splunk.License --dry-run -o yaml | kubectl apply -f -
 # Install Splunk Operator
 kubectl apply -f $SPLUNKOPERATORURL
 kubectl wait --for condition="established" crd --all
+# Create namespace
+# kubectl create namespace splunk --dry-run -o yaml | kubectl apply -f -
+cat <<EOF | kubectl apply -f -
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: splunk
+---
+kind: RoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: splunk:operator:namespace-manager
+  namespace: splunk
+subjects:
+- kind: ServiceAccount
+  name: splunk-operator
+  namespace: splunk-operator
+roleRef:
+  kind: ClusterRole
+  name: splunk:operator:namespace-manager
+  apiGroup: rbac.authorization.k8s.io
+EOF
+# Add license to configmap
+kubectl create configmap --namespace splunk splunk-licenses --from-file=/tmp/Splunk.License --dry-run -o yaml | kubectl apply -f -
 # Install Splunk
 kubectl apply -f $SPLUNKDEPLOYMENTYAML
